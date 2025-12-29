6b7caf5c7caa55472534be96b694c81ec885533d
BorisenkoGeorgy <92270694+BorisenkoGeorgy@users.noreply.github.com>
2025-12-29 18:54:39 +0300

    first experiment

--- models/code.py
+++ models/code.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, hid_dim):
         super().__init__()
         self.conv_module = nn.Sequential(
             *[
-                nn.Conv1d(2053, 16, 5),
+                nn.Conv1d(hid_dim, 16, 5),
                 nn.BatchNorm1d(16),
                 nn.ReLU(inplace=True),
                 nn.Conv1d(16, 32, 5),
@@ -24,11 +24,11 @@ class CNN(nn.Module):
 
 class CRNN(nn.Module):
 
-    def __init__(self):
+    def __init__(self, hid_dim):
         super().__init__()
         self.conv_module = nn.Sequential(
             *[
-                nn.Conv1d(5, 16, 3, padding=1),
+                nn.Conv1d(hid_dim, 16, 3, padding=1),
                 nn.BatchNorm1d(16),
                 nn.ReLU(inplace=True),
                 nn.Conv1d(16, 32, 3, padding=1),
@@ -36,20 +36,20 @@ class CRNN(nn.Module):
             ]
         )
         self.rnn = nn.LSTM(32, 64, 2, batch_first=True)
-        self.proj = nn.Linear(64, 5)
+        self.proj = nn.Linear(128, 5)
 
     def forward(self, x: torch.Tensor):
         x = self.conv_module(x.transpose(1, 2))
-        out, _ = self.rnn(x)
-        return self.proj(out[:,-1,:])
+        _, (h_n, _) = self.rnn(x.transpose(1, 2))
+        return self.proj(h_n.view(h_n.shape[1], -1))
     
 class CBiRNN(nn.Module):
 
-    def __init__(self):
+    def __init__(self, hid_dim):
         super().__init__()
         self.conv_module = nn.Sequential(
             *[
-                nn.Conv1d(5, 16, 3, padding=1),
+                nn.Conv1d(hid_dim, 16, 3, padding=1),
                 nn.BatchNorm1d(16),
                 nn.ReLU(inplace=True),
                 nn.Conv1d(16, 32, 3, padding=1),
@@ -57,22 +57,20 @@ class CBiRNN(nn.Module):
             ]
         )
         self.rnn = nn.LSTM(32, 64, 2, batch_first=True, bidirectional=True)
-        self.proj = nn.Linear(128, 5)
+        self.proj = nn.Linear(256, 5)
 
     def forward(self, x: torch.Tensor):
-        x = self.conv_module(x).transpose(1, 2)
-        out, _ = self.rnn(x)
-        out = out.reshape(*x.shape[:2], 64, 2)
-        out = torch.cat([out[:, -1, :, 0], out[:, 0, :, 1]],dim=-1)
-        return self.proj(out)
+        x = self.conv_module(x.transpose(1, 2))
+        _, (h_n, _) = self.rnn(x.transpose(1, 2))
+        return self.proj(h_n.view(h_n.shape[1], -1))
     
 class CTransformer(nn.Module):
 
-    def __init__(self):
+    def __init__(self, hid_dim):
         super().__init__()
         self.conv_module = nn.Sequential(
             *[
-                nn.Conv1d(5, 16, 3, padding=1),
+                nn.Conv1d(hid_dim, 16, 3, padding=1),
                 nn.BatchNorm1d(16),
                 nn.ReLU(inplace=True),
                 nn.Conv1d(16, 32, 3, padding=1),
@@ -88,5 +86,5 @@ class CTransformer(nn.Module):
 
     def forward(self, x: torch.Tensor):
         x = self.conv_module(x.transpose(1, 2))
-        out = self.transformer(x).mean(1)
+        out = self.transformer(x.transpose(1, 2)).mean(1)
         return self.proj(out)
